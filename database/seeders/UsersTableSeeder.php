28e42697c546ece90a340a7cb3552aff426d549d
Quan Vu <info@quanvu.net>
2020-11-28 00:10:51 +0700

    Seeder default User with personal team

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Models\Team;
 
 class UsersTableSeeder extends Seeder
 {
@@ -16,10 +17,16 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         // Create default user
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@laravel8.com',
             'password' => Hash::make('Admin@123')
+        ]);
+
+        Team::create([
+            'user_id' => $user->id,
+            'name' => $user->name."'s Team",
+            'personal_team' => $user->id,
         ]);
     }
 }
